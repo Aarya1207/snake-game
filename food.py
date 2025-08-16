75145e12eb7af2154250f9f68001891efca1d1b1
Aarya1207 <aaryadhope12@gmail.com>
2025-08-16 16:10:00 +0530

    Initial commit - Snake Game

---
+++ food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)  # make it smaller
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        """Move the food to a new random location"""
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
